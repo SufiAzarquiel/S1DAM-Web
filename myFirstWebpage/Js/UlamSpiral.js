af2c5c041e065aa67fb5cc583b5e84d75ce4b785
SufiAzarquiel <alumno.745709@ies-azarquiel.es>
2022-10-20 13:10:36 +0200

    Update UlamSpiral.js
    Draw all positions of the spiral instead of just one every frame.

--- myFirstWebpage/Js/UlamSpiral.js
+++ myFirstWebpage/Js/UlamSpiral.js
@@ -7,6 +7,7 @@
  */
 // Definition copied from https://en.wikipedia.org/wiki/Ulam_spiral.
 
+// Set canvas
 "use strict";
 var canvas = document.querySelector("canvas");
 canvas.width = 640;
@@ -18,15 +19,22 @@ var bgColor = "#69fd9a";
 var bgRect = { x: 0, y: 0, w: 640, h: 640 };
 
 // Spiral variables
-var length = 3;
 var step = 0;
-var dS = 0;
+// Step size
+const dS = 30;
+// Length of the spiral
+const spiralLength = 3;
+var spiral = new Array(length);
 
 // Set starting position at center of canvas
 const cX = canvas.width / 2;
 const cY = canvas.height / 2;
+
+// Current position on the spiral
 var x = 0;
 var y = 0;
+
+// Previous position on the spiral
 var px = 0;
 var py = 0;
 
@@ -34,29 +42,20 @@ var py = 0;
 const circleRad = 10;
 const circleColor = "#000000";
 
-// Keep track of current frame
-g.font = "20px Georgia";
+// Variables for a timer that keeps track of the current step in the loop
 var timer = 0;
 var gen = 0;
 
-// Main loop variables
-var loopDelay = 100;
+// Delay between every step on the loop
+const loopDelay = 100;
+// Main loop
 var loopInterval;
 
+// Function for drawing rectangles to the screen
 function fillRect(r, color) {
   g.fillStyle = color;
   g.fillRect(r.x, r.y, r.w, r.h);
 }
-
-// Draw starting screen
-fillRect(bgRect, bgColor);
-
-// Set initial state
-x = cX;
-y = cY;
-px = cX;
-py = cY;
-dS = 30;
 
 // Function for drawing a circle at a given position
 function drawCircle(x, y) {
@@ -87,28 +86,56 @@ function drawLine(x1, y1, x2, y2) {
   g.stroke();
 }
 
+// Set all the values of the spiral to be 0
+function clearSpiral(arr) {
+  // Assign all values in spiral to 0
+  for (let i = 0; i < spiralLength; i++) {
+    arr[i] = 0;
+  }
+  return arr;
+}
+
+// Initialize program before entering loop
+// Set font for the text used
+g.font = "20px Georgia";
+
+// Draw starting screen
+fillRect(bgRect, bgColor);
+
+// Set initial state
+px = cX;
+py = cY;
+
+// Initialize the spiral
+spiral = clearSpiral(spiral);
+
 // Enter game loop
 function main() {
   // Draw the background
   fillRect(bgRect, bgColor);
 
-  // Update position with a condition
-  if (step < length) {
-    // Update x and y
-    x += dS;
-    y += dS;
+  // Go to next position if we aren't at the end of the spiral
+  if (step < spiralLength) {
+    // Set current position in the spiral to be drawn
+    spiral[step] = 1;
     step++;
   }
 
-  // Draw a circle to the screen
-  drawCircle(x, y);
-
-  // Draw line from current point to previous point
-  drawLine(x, y, px, py);
-
-  // Set previous point to be this point after completing the drawing step
-  px = x;
-  py = y;
+  // Draw the current state of the spiral
+  // Iterate through all elements in spiral
+  for (let i = 0; i < spiralLength; i++) {
+    // If current position on the spiral is not empty
+    if (spiral[i] == 1) {
+      // Calculate (x, y) position of the circle to be drawn
+      x = cX + i * dS;
+      y = cY + i * dS;
+      drawCircle(x, y);
+      drawLine(x, y, px, py);
+      // Set previous point to be this point after completing the drawing step
+      px = x;
+      py = y;
+    }
+  }
 
   // Keep track of the current step on the screen.
   timer++;
@@ -116,10 +143,12 @@ function main() {
   document.getElementById("genCount").innerHTML = gen;
 }
 
+// Stop loop on any state
 function stop() {
   clearInterval(loopInterval);
 }
 
+// Start loop if it hasn't been started yet
 function start() {
   if (loopInterval != null) {
     clearInterval(loopInterval);
@@ -127,7 +156,7 @@ function start() {
   loopInterval = setInterval(main, loopDelay);
 }
 
-// Reset progress
+// Reset loop state
 function reset() {
   // Draw the background
   fillRect(bgRect, bgColor);
@@ -138,6 +167,9 @@ function reset() {
   px = cX;
   py = cY;
   step = 0;
+
+  // Reset spiral values to 0
+  spiral = clearSpiral(spiral);
 
   // Reset the counter for generations
   timer = 0;
